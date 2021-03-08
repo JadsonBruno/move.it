eaa8e39e8fa09348267a1eda8cc95b4289c09216
Jadson <jadsombruno2016@gmail.com>
2021-03-08 19:07:36 -0300

    Define basic nextjs configuration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+/**
+ * IMPORTS
+ */
+import '../styles/global.css';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I render the app component.
+ *
+ * returns: app component
+ */
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+
+/**
+ * EXPORTS
+ */
+export default MyApp
