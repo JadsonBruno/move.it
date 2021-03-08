33dc5a63ea657850ec5cbd58cc18478672d1fc4a
Jadson <jadsombruno2016@gmail.com>
2021-03-08 19:18:15 -0300

    Create level up modal component

---
+++ src/components/LevelUpModal/index.tsx
@@ -0,0 +1,55 @@
+/**
+ * IMPORTS
+ */
+import {useContext} from 'react';
+import {ChallengesContext} from '../../contexts/ChallengesContext';
+import styles from '../../styles/components/LevelUpModal.module.css';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I render the level up modal component.
+ *
+ * returns: level up modal component
+ */
+function LevelUpModal ()
+{
+    // get data from context
+    const {level, closeLevelUpModal} = useContext(ChallengesContext);
+
+    // return component
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <header>
+                    {level}
+                </header>
+                <strong>
+                    Parabéns
+                </strong>
+
+                <p>
+                    Você alcançou um novo level.
+                </p>
+
+                <button 
+                    type="button"
+                    onClick={closeLevelUpModal}
+                >
+                    <img src="/icons/close.svg" alt="Fechar modal"/>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+
+/**
+ * EXPORTS
+ */
+export {
+    LevelUpModal
+}
