a11d467e9b8bb310ae89236e980014f7d2188ce3
Jadson <jadsombruno2016@gmail.com>
2021-03-08 19:17:58 -0300

    Create experience bar component

---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,51 @@
+/**
+ * IMPORTS
+ */
+import {useContext} from 'react';
+import {ChallengesContext} from '../../contexts/ChallengesContext';
+import styles from '../../styles/components/ExperienceBar.module.css';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I render the experience bar component.
+ *
+ * returns: experience bar component
+ */
+function ExperienceBar ()
+{
+    // get data from context
+    const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
+
+    // format data to percentage
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
+    // return component
+    return (
+        <header className={styles.experienceBar}>
+            <span>8 xp</span>
+            <div>
+                <div style={{width: `${percentToNextLevel}%`}}/>
+
+                <span
+                    className={styles.currentExperience}
+                    style={{left: `${percentToNextLevel}%`}}
+                >
+                    {currentExperience} xp
+                </span>
+            </div>
+            <span>{experienceToNextLevel} xp</span>
+        </header>
+    );
+}
+
+
+/**
+ * EXPORTS
+ */
+export {
+    ExperienceBar
+}
